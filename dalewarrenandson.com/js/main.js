a491768b04a0fe86c78801833af602d29a66aa3b
Bren Warren <brenwarren@me.com>
2014-03-04 22:39:59 +0100

    added something-not-listed popover in woodburner section

--- dalewarrenandson.com/js/main.js
+++ dalewarrenandson.com/js/main.js
@@ -5,3 +5,6 @@
 
 //  THIS IS FOR THE SERVICE AREA POPOVER - "Not sure if your location is covered?"
 $('#service-area-popover').popover();
+
+// THIS IS FOR THE WOODBURNERS POPOVER - 
+$('#woodburners-popover').popover();
